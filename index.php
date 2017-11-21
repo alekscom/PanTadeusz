1ecf1e9d396841b54e143197b45149712cd2d0fb
alekscom <aleks.griaznowa@gmail.com>
2017-11-21 11:35:05 +0100

    z notatkami

--- index.php
+++ index.php
@@ -14,29 +14,69 @@
 		historia szlachecka z roku 1811 i 1812 we dwunastu księgach wierszem
 		</h1>
 	</header>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top mb-2 rounded">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top mb-2 rounded ">
 		<div class="">
-			<a class="navbar-brand" href="./index.php"> Strona główna</a>
+			<?php 
+				$k = $_GET['k'];
+				echo "
+			<a class='navbar-brand' href='./index.php'> Strona główna</a>
+			<a class='navbar-brand' href='./index.php?k=$k&n=dodaj'> Dodaj notatkę</a>
+			<a class='navbar-brand' href='./index.php?k=$k&n=lista'> Lista notatek</a>
+			";
+			?>
 		</div>
 	</nav>
-<div class="container mb-5">
+	<div class="container mb-5">
 		<div class="row">
-			<div class="col-md-3 col-sm-12">
+			<div class="col-md-4 col-sm-12">
 				<div class="list-group">
 					<button class='list-group-item list-group-item-info'>Spis treści</button>
 					<?php
 						for ($i=1; $i <= 12; $i++) { 
-							echo "<a class='list-group-item list-group-item-action' href='./index.php?ksiega=$i'>Księga $i</a>";
+							echo "<a class='list-group-item list-group-item-action' href='./index.php?k=$i'>Księga $i</a>";
 						}
 					?>
 				</div>
 			</div>
-			<div class="col-md-9 col-sm-12">
+			<div class="col-md-8 col-sm-12">
 				<?php
+				if(isset($_GET['n'])) {
+					$n = $_GET['n'];
+					if($n == "dodaj"){
+						echo "<form action='' method='POST'>
+								<div class='form-group'>
+								<label for='title'>Tytul notatki</label>
+								<input type='text' class='form-control' 
+								id='title' name='title'></div>
+								<div class='form-group'> 
+								<label for='content'>Treść notatki</label>
+								<textarea class='form-control' name='content' id='content' rows='3'></textarea>
+								</div>
+							<button type='submit' class='btn btn-info'>Submit</button>
+								</form>";
+					} elseif($n == "lista"){
+						$k = $_GET['k'];
+						echo '<table class="table table-hover">
+								<thead>
+									<tr>
+										<th scope="col">#</th>
+										<th scope="col">Tytul</th>
+										<th scope="col">Treść</th>
+									</tr>
+								</thead>
+								<tbody>
+								<tr>
+									<th scope="row">1</th>
+									<td>Przykładowy tytuł</td>
+									<td>Przykład notatki</td>
+								</tr>
+								</tbody>
+							</table>';
+					}
+				}
 					if (!empty($_GET)) {
-						$k = $_GET['ksiega'];
-			   			include_once("./k$k.html");
-			   			echo "<a href='#' class='btn btn-info btn-lg back-to-top sticky-top float-right' role='button'>UP</a>";
+						$k = $_GET['k'];
+			   			require_once("./k$k.html");
 					} else {
 						echo "<img src='./pantadeusz.jpg' class='rounded mx-auto d-block border border-info p-2' >";
 					}
@@ -46,6 +86,9 @@
 	</div>
 
 	<footer class="container">
-	<p class="fixed-bottom alert alert-success"> (c) Oleksandra Hriaznova</p></footer>
+	<p class="fixed-bottom alert alert-success"> (c) Oleksandra Hriaznova 2017 Kraków</p>
+	</footer>
+	
+
 </body>
 </html>
